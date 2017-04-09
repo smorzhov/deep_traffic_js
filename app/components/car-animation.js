b42cd2fe251e9d5816bb25cda12f5832bfabecf9
MariaSopkova <lisica.m@mail.ru>
2017-04-10 01:05:13 +0300

    Обновление машин
    Обновление машин

--- app/components/car-animation.js
+++ app/components/car-animation.js
@@ -2,21 +2,44 @@ import Ember from 'ember';
 import Traffic from './../deep_traffic/traffic/traffic';
 
 export default Ember.Component.extend({
-  actions: {
-    update() {
-      let traffic = new Traffic();
-      this.set('traffic', traffic);
-      this.traffic.update();
-      this.traffic.update();
-      this.traffic.update()
+  traffic: Ember.inject.service('traffic'),
+  showCount: 1,
+  cars:[],
+  init: function() {
+    this._super();
+    // Update the time.
+    this.set('cars',this.get('traffic').getCarsToShow());
+    //this.updateCount();
+  },
+
+  updateCount: function() {
+    let _this = this;
+    this.set('showCount', this.get('traffic').getCount());
+    Ember.run.later(this, function() {
+      _this.get('traffic').update();
+      _this.set('cars',this.get('traffic').getCarsToShow());
+      _this.updateCount();
+    }, 1000);
+  },
+  /*actions: {
+    show() {
+      this.set('showCount', this.get('traffic').getCount());
+      Ember.run.later(this, function() {
+        this.get('traffic').setCount();
+        this.set('showCount', this.get('traffic').getCount());
+        this.show();
+      }, 100);
+    }
+  }*/
+
+  /*updateCars(){
+    this.get('traffic').update();
+    this.set('cars',this.get('traffic').getCarsToShow());
+  },*/
+  actions:{
+    updateCars(){
+      this.get('traffic').update();
+      this.set('cars',this.get('traffic').getCarsToShow());
     }
   }
-  /*init() {
-    this._super();
-    let traffic = new Traffic();
-    this.set('traffic', traffic);
-    this.traffic.update();
-    this.traffic.update();
-    this.traffic.update();
-  }*/
 });
