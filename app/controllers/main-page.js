d7f302d9845f49fe09c6b456ab87242a1894a52e
Ksenia Lagutina <lagutinakv@mail.ru>
2017-04-10 00:01:33 +0300

    Add sending user results without checking

--- app/controllers/main-page.js
+++ app/controllers/main-page.js
@@ -23,6 +23,13 @@ export default Ember.Controller.extend({
         this.set("userInputData", true);
     },
     submitData() {
+        let userEmail = this.get('userEmail');
+        let userNick = this.get('userNick');
+        $.ajax({
+            type: "POST",
+            url: "http://deep-traffic-server.azurewebsites.net/api/users",
+            data: { mph: 42, email: userEmail, user: userNick }
+        });
         this.set("userInputData", false);
     }
   }
