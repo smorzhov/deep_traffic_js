8695d118238b0c396eaa7bf945065208049193a9
MariaSopkova <lisica.m@mail.ru>
2017-04-09 15:40:19 +0300

    Контроллер main-page
    main-page

---
+++ app/controllers/main-page.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import Traffic from './../deep_traffic/traffic/traffic';
+
+export default Ember.Controller.extend({
+  //traffic: new Traffic(),
+  init(){
+    this._super(...arguments);
+    let traffic = new Traffic();
+    this.set('traffic', traffic);
+    this.traffic.update();
+  },
+  /*reopen(){
+   let traffic = new Traffic();
+   this.set('traffic', traffic);
+   this.traffic.update();
+   },*/
+  actions: {
+    updateTraffic() {
+      this.traffic.update();
+    }
+  }
+});
