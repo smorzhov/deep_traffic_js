035f4fd7a73672e5cba8acba804470b0cc493c15
smorzhov <smorzhov@gmail.com>
2017-04-10 21:15:47 +0300

    Added car images. Minor changes.

--- app/components/car-animation.js
+++ app/components/car-animation.js
@@ -2,48 +2,48 @@ import Ember from 'ember';
 import Traffic from './../deep_traffic/traffic/traffic';
 
 export default Ember.Component.extend({
-  traffic: Ember.inject.service('traffic'),
-  showCount: 1,
-  cars:[],
-  overtakenCars:0,
-  init: function() {
-    this._super();
-    // Update the time.
-    this.set('cars',this.get('traffic').getCarsToShow());
-    this.set('overtakenCars', this.get('traffic').getOvertakenCars());
-    //this.updateCount();
-  },
+    traffic: Ember.inject.service('traffic'),
+    showCount: 1,
+    cars: [],
+    overtakenCars: 0,
+    init: function () {
+        this._super();
+        // Update the time.
+        this.set('cars', this.get('traffic').getCarsToShow());
+        this.set('overtakenCars', this.get('traffic').getOvertakenCars());
+        //this.updateCount();
+    },
 
-  updateCount: function() {
-    let _this = this;
-    this.set('showCount', this.get('traffic').getCount());
-    Ember.run.later(this, function() {
-      _this.get('traffic').update();
-      _this.set('cars',this.get('traffic').getCarsToShow());
-      _this.updateCount();
-    }, 1000);
-  },
-  /*actions: {
-    show() {
-      this.set('showCount', this.get('traffic').getCount());
-      Ember.run.later(this, function() {
-        this.get('traffic').setCount();
+    updateCount: function () {
+        let _this = this;
         this.set('showCount', this.get('traffic').getCount());
-        this.show();
-      }, 100);
-    }
-  }*/
+        Ember.run.later(this, function () {
+            _this.get('traffic').update();
+            _this.set('cars', this.get('traffic').getCarsToShow());
+            _this.updateCount();
+        }, 1000);
+    },
+    /*actions: {
+      show() {
+        this.set('showCount', this.get('traffic').getCount());
+        Ember.run.later(this, function() {
+          this.get('traffic').setCount();
+          this.set('showCount', this.get('traffic').getCount());
+          this.show();
+        }, 100);
+      }
+    }*/
 
-  /*updateCars(){
-    this.get('traffic').update();
-    this.set('cars',this.get('traffic').getCarsToShow());
-  },*/
-  actions:{
-    updateCars(){
+    /*updateCars(){
       this.get('traffic').update();
       this.set('cars',this.get('traffic').getCarsToShow());
-      let allOverTaken = this.get('overtakenCars');
-      this.set('overtakenCars', allOverTaken + this.get('traffic').getOvertakenCars());
+    },*/
+    actions: {
+        updateCars() {
+            this.get('traffic').update();
+            this.set('cars', this.get('traffic').getCarsToShow());
+            let allOverTaken = this.get('overtakenCars');
+            this.set('overtakenCars', allOverTaken + this.get('traffic').getOvertakenCars());
+        }
     }
-  }
 });
--- app/controllers/main-page.js
+++ app/controllers/main-page.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 import Traffic from './../deep_traffic/traffic/traffic';
 
 export default Ember.Controller.extend({
-  //traffic: new Traffic(),
-  init(){
-    this._super(...arguments);
-    let traffic = new Traffic();
-    this.set('traffic', traffic);
-    this.traffic.update();
-  },
-  actions: {
-  }
+    //traffic: new Traffic(),
+    init() {
+        this._super(...arguments);
+        let traffic = new Traffic();
+        this.set('traffic', traffic);
+        this.traffic.update();
+    },
+    actions: {
+    }
 });
