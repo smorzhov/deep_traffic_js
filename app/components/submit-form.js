f8fe33047ddcb6a5fb1a364a258ad8ade8ebb037
Ksenia Lagutina <lagutinakv@mail.ru>
2017-04-10 20:36:32 +0300

    Add user input validation

--- app/components/submit-form.js
+++ app/components/submit-form.js
@@ -2,17 +2,42 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     userInputData: false,
+    emailIsInvalide: false,
+    nickIsInvalide: false,
+    validateInputData() {
+        let regExpForEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        let userEmail = this.get('userEmail');
+        let userNick = this.get('userNick');
+        if (userNick === undefined || userNick.trim() === "") {
+            this.set("nickIsInvalide", true);
+        } else {
+            this.set("nickIsInvalide", false);
+        }
+        if (userEmail === undefined || userEmail.trim() === "" || !regExpForEmail.test(userEmail)) {
+            this.set("emailIsInvalide", true);
+        } else {
+            this.set("emailIsInvalide", false);
+        }
+    },
     actions: {
         showForm() {
+            this.set("nickIsInvalide", false);
+            this.set("emailIsInvalide", false);
             this.set("userInputData", true);
+            this.set('userEmail', "");
+            this.set('userNick', "");
         },
         submitData() {
+            this.validateInputData();
+            if (this.get("emailIsInvalide") || this.get("nickIsInvalide")) {
+                return;
+            }
             let userEmail = this.get('userEmail');
             let userNick = this.get('userNick');
             $.ajax({
                 type: "POST",
                 url: "http://deep-traffic-server.azurewebsites.net/api/users",
-                data: { mph: 42, email: userEmail, user: userNick }
+                data: { mph: 44, email: userEmail, user: userNick }
             });
             this.set("userInputData", false);
         },
--- app/controllers/main-page.js
+++ app/controllers/main-page.js
@@ -10,18 +10,5 @@ export default Ember.Controller.extend({
     this.traffic.update();
   },
   actions: {
-    showForm() {
-      this.set("userInputData", true);
-    },
-    submitData() {
-      let userEmail = this.get('userEmail');
-      let userNick = this.get('userNick');
-      $.ajax({
-        type: "POST",
-        url: "http://deep-traffic-server.azurewebsites.net/api/users",
-        data: {mph: 42, email: userEmail, user: userNick}
-      });
-      this.set("userInputData", false);
-    }
   }
 });
