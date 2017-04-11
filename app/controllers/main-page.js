5f83d22f397d609251690b1e3d3dfc0705ffaef4
smorzhov <smorzhov@gmail.com>
2017-04-11 22:52:38 +0300

    Added file pickes for loading and saving.

--- app/controllers/main-page.js
+++ app/controllers/main-page.js
@@ -1,14 +1,42 @@
 import Ember from 'ember';
-import Traffic from './../deep_traffic/traffic/traffic';
 
 export default Ember.Controller.extend({
-    //traffic: new Traffic(),
     init() {
         this._super(...arguments);
-        let traffic = new Traffic();
-        this.set('traffic', traffic);
-        //this.traffic.update();
     },
     actions: {
+        saveCode() {
+            const { dialog } = require('electron').remote;
+            const fs = require('fs');
+            dialog.showSaveDialog({
+                filters: [{ name: 'JavaScript file', extensions: ['js'] }]
+            }, fileName => {
+                if (fileName === undefined) {
+                    return;
+                }
+                fs.writeFile(fileName, monaco.editor.getModels()[0].getValue(), error => {
+                    console.log(error);
+                });
+            });
+        },
+        loadCode() {
+            const { dialog } = require('electron').remote;
+            const fs = require('fs');
+            dialog.showOpenDialog({
+                filters: [{ name: 'JavaScript file', extensions: ['js'] }],
+                openFile: true,
+                multiSelections: false
+            }, fileName => {
+                if (fileName === undefined) {
+                    return;
+                }
+                fs.readFile(fileName[0], 'utf8', (error, code) => {
+                    if (error) {
+                        return console.log(error);
+                    }
+                    monaco.editor.getModels()[0].setValue(code);
+                });
+            });
+        }
     }
 });
